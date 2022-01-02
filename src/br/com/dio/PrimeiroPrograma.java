bbd3f38c7a0bd9f07d71a385d36eed304910be1e
bootcamprepo <mysteamlinux@outlook.com>
2022-01-02 16:42:42 -0300

    Class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.animal.Animal;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
@@ -12,6 +14,57 @@ public class PrimeiroPrograma {
         Animal animal = new Animal("Dog", "Dogui", 5);
 
         System.out.println(animal);
+
+        Livro livro = new Livro("Java",355);
+        System.out.println(livro);
+
+
+    }
+}
+
+class Livro{
+    String nome;
+    Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return nome.equals(livro.nome) && numPaginas.equals(livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
 }
